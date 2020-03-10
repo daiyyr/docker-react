c513de006d93e62f224505d4bd3f51eddf3f08a6
daiyyr <54739633@qq.com>
2020-03-11 01:38:59 +1300

    changed app text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Lucy.
-          How are you?
-          I miss you
+          I was changed on the feature branch
         </p>
         <a
           className="App-link"
