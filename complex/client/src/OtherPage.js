e53920e95f2437aa1949b31d193a29ad2c984882
daiyyr <54739633@qq.com>
2020-03-13 17:41:24 +1300

    project complex as Multi-Container Application

---
+++ complex/client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// eslint-disable-next-line
+import {Link} from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            Im some other page!
+            <link to="/">Go back home</link>
+        </div>
+    );
+};
