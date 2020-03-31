32de5b818b6cbd270d87baba880eb8780314f1c1
daiyyr <54739633@qq.com>
2020-04-01 11:41:00 +1300

    try travis

---
+++ complex_k8s/deploy.sh
@@ -0,0 +1,15 @@
+docker build -t teemo/multi-client:latest -t teemo/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t teemo/multi-server:latest -t teemo/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t teemo/multi-worker:latest -t teemo/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push teemo2020/multi-client:latest
+docker push teemo2020/multi-client:$SHA
+docker push teemo2020/multi-server:latest
+docker push teemo2020/multi-server:$SHA
+docker push teemo2020/multi-worker:latest
+docker push teemo2020/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployment/server-deployment server=teemo2020/multi-server:$SHA
+kubectl set image deployment/client-deployment client=teemo2020/multi-client:$SHA
+kubectl set image deployment/worker-deployment worker=teemo2020/multi-worker:$SHA
+
+
